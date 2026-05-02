829d5bdc021567165fb019fff2a8cca627df1d62
l30 <leito.escorcia@gmail.com>
2026-05-02 02:58:25 -0500

    Solucion error uvicorn

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ mail_conf = ConnectionConfig(
     MAIL_FROM=os.getenv("EMAIL_FROM"),
     MAIL_PORT=587,
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_TLS=True,
-    MAIL_SSL=False,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
 )
 
 fm = FastMail(mail_conf)
