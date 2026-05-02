6ffcd79b99b7f06c005dcc8e51421de437b5d5ae
l30 <leito.escorcia@gmail.com>
2026-05-01 22:55:47 -0500

    Se arregla boton cancelar

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routes import students
 from database import engine, Base
+
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
 app.include_router(students.router)
