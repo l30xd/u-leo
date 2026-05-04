db3203fd99f5842a2b333d0311f9e809864ab1ac
l30 <leito.escorcia@gmail.com>
2026-05-04 12:12:37 -0500

    error secret file render 2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ENV_PATH = "/etc/secrets/.env" if os.path.exists("/etc/secrets/.env") else os.path.join(BASE_DIR, ".env")
+ENV_PATH = "/etc/secrets/env" if os.path.exists("/etc/secrets/env") else os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path=ENV_PATH)
 
 Base.metadata.create_all(bind=engine)
